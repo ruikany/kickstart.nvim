cb1a71bf3f9c3099918c5325e3aa3dc85775da4b
ruikany <dustin04dd@gmail.com>
2026-04-02 12:37:31 -0400

    trying to setup ai

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,39 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('codecompanion').setup {
+        adapters = {
+          gemini = function()
+            return require('codecompanion.adapters').extend('gemini', {
+              env = {
+                api_key = os.getenv 'GEMINI_API_KEY',
+              },
+              schema = {
+                model = {
+                  -- Explicitly use Flash (Free Tier friendly)
+                  default = 'gemini-1.5-flash',
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = { adapter = 'gemini' },
+          inline = { adapter = 'gemini' },
+          agent = { adapter = 'gemini' },
+        },
+      }
+      -- Keymaps for the sidebar and inline actions
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', '<cmd>CodeCompanionChat Toggle<cr>', { desc = '[C]ode [A]ssistant Chat' })
+      vim.keymap.set({ 'n', 'v' }, '<leader>ce', '<cmd>CodeCompanionActions<cr>', { desc = '[C]ode [E]dit Actions' })
+    end,
+  },
+}
